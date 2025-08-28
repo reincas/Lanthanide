f4beb222cf83441aa6b00bc94475df033cbdea67
reincas <r.caspary@posteo.de>
2025-08-28 19:35:35 +0200

    Update test_wigner.py

--- test/test_wigner.py
+++ test/test_wigner.py
@@ -1,3 +1,9 @@
+##########################################################################
+# Copyright (c) 2025 Reinhard Caspary                                    #
+# <reinhard.caspary@phoenixd.uni-hannover.de>                            #
+# This program is free software under the terms of the MIT license.      #
+##########################################################################
+
 import math
 import pytest
 
